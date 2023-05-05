9de2e5e8bf995e7c32ee92281fc88b847285351b
fauzandp <fauzan27dp@gmail.com>
2023-05-05 07:21:56 +0700

    Project2 | Guess Flag add highscore to the game add local storage with UserDefaults

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -19,7 +19,19 @@ class ViewController: UIViewController {
     var countries = [String]()
     var score = 0
     var correctAnswer = 0
-    var tap = 0
+    var defaults = UserDefaults.standard
+    
+    var tap = 10 {
+        didSet {
+            navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Life: \(tap)")
+        }
+    }
+    
+    var highScore = 0 {
+        didSet {
+            navigationItem.leftBarButtonItem = UIBarButtonItem(title: "HS: \(highScore)")
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,7 +47,29 @@ class ViewController: UIViewController {
         button3.layer.borderColor = UIColor.lightGray.cgColor
         
         askQuestion()
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: String(tap), style: .plain, target: .none, action: .none)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Life: \(tap)")
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "HS: \(highScore)")
+        
+        if let savedData = defaults.object(forKey: "highScore") as? Data {
+            let jsonDecoder = JSONDecoder()
+            
+            do {
+                highScore = try jsonDecoder.decode(Int.self, from: savedData)
+            } catch {
+                print("Failed to load highscore.")
+            }
+        }
+    }
+    
+    func saveData() {
+        let jsonEncoder = JSONEncoder()
+        
+        do {
+            let savedData = try jsonEncoder.encode(highScore)
+            defaults.set(savedData, forKey: "highScore")
+        } catch {
+            print("Failed to save highscore.")
+        }
     }
 
     func askQuestion(action: UIAlertAction! = nil) {
@@ -52,46 +86,55 @@ class ViewController: UIViewController {
     @IBAction func buttonTapped(_ sender: UIButton) {
         var title: String
 
-        tap += 1
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: String(tap), style: .plain, target: .none, action: .none)
+        tap -= 1
         
         if sender.tag == correctAnswer {
             title = "Correct"
             score += 1
         } else {
             title = "Wrong! That's the flag of \(countries[sender.tag].uppercased())"
-            score -= 1
+            
+            if score > 0 {
+                score -= 1
+            }
         }
         
         liveScore.text = "Score : \(score)"
         
         var msg = "Your score is \(score)"
-        
-        if tap == 10 {
+        if tap == 0 {
             msg = "Your final score is \(score)"
-        }
-        
-        let ac = UIAlertController(title: title, message: msg, preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
-        
-        present(ac, animated: true)
-        
-        if tap == 10 {
+            
+            isHighScore(score)
+            
             button1.isEnabled = false
             button2.isEnabled = false
             button3.isEnabled = false
         }
+        
+        let ac = UIAlertController(title: title, message: msg, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        present(ac, animated: true)
+        
+    }
+    
+    func isHighScore(_ score: Int) {
+        if score > highScore {
+            highScore = score
+            saveData()
+        }
+        
+        let ac = UIAlertController(title: "New HighScore", message: "\(highScore)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
     }
     
     @IBAction func resetTapped(_ sender: UIButton) {
-        tap = 0
+        tap = 10
         score = 0
+        liveScore.text = "Score : \(score)"
         
         askQuestion()
-        
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: String(tap), style: .plain, target: .none, action: .none)
-        
-        liveScore.text = "Score : \(score)"
         
         button1.isEnabled = true
         button2.isEnabled = true
